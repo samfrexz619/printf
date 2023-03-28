425a35a85f21b0c55ce284c79504549aacc0af3f
Bright Akin <akinkuotubright@gmail.com>
2023-03-28 07:41:19 -0700

    Update print_short_hex.c

--- print_short_hex.c
+++ print_short_hex.c
@@ -5,6 +5,7 @@
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
  * Return: number of chars printed
+ * print_short_hex.c
  */
 int prinhhex(va_list arguments, char *buf, unsigned int ibuf)
 {
