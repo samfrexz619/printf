237e477f907e86af3aaf6be55ff3c3368c7e7432
Bright Akin <akinkuotubright@gmail.com>
2023-03-28 07:36:46 -0700

    Update print_num_hex.c

--- print_num_hex.c
+++ print_num_hex.c
@@ -5,6 +5,7 @@
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
  * Return: number of chars printed
+ * print_num_hex.c
  */
 int prinnhex(va_list arguments, char *buf, unsigned int ibuf)
 {
