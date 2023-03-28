35c694bf236c44329d001991a09ea678b77582f8
Bright Akin <akinkuotubright@gmail.com>
2023-03-28 07:35:53 -0700

    Update print_long_upx.c

--- print_long_upx.c
+++ print_long_upx.c
@@ -5,6 +5,7 @@
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
  * Return: number of chars printed
+ * print_long_upx.c
  */
 int prinlupx(va_list arguments, char *buf, unsigned int ibuf)
 {
