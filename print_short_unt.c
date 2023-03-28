c4f62b4ad7df1360b96e4fbf94057a8a13114ecb
Bright Akin <akinkuotubright@gmail.com>
2023-03-28 07:44:20 -0700

    Update print_short_unt.c

--- print_short_unt.c
+++ print_short_unt.c
@@ -5,6 +5,7 @@
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
  * Return: number of chars printed.
+ * print_short_unt.c
  */
 int prinhunt(va_list arguments, char *buf, unsigned int ibuf)
 {
