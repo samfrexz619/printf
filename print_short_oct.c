8391aa4d7c01ea107a8675d45515a774d4303a50
Bright Akin <akinkuotubright@gmail.com>
2023-03-28 07:43:30 -0700

    Update print_short_oct.c

--- print_short_oct.c
+++ print_short_oct.c
@@ -5,6 +5,7 @@
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
  * Return: number of chars printed.
+ * print_short_oct.c
  */
 int prinhoct(va_list arguments, char *buf, unsigned int ibuf)
 {
