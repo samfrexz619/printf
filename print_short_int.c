97d3ac7dd01f8e5c38a0fec2f9785de6ac3af3a5
Bright Akin <akinkuotubright@gmail.com>
2023-03-28 07:42:28 -0700

    Update print_short_int.c

--- print_short_int.c
+++ print_short_int.c
@@ -5,6 +5,7 @@
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
  * Return: number of chars printed.
+ * print_short_int.c
  */
 int prinhint(va_list arguments, char *buf, unsigned int ibuf)
 {
